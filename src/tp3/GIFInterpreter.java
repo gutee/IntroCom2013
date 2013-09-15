bcae6ffe1d57b5df7b9d0c6b12b8a15e6aaa17c5
Martin Gutierrez <gutix007@hotmail.com>
2013-09-15 15:30:38 -0300

    wipuu

--- src/tp3/GIFInterpreter.java
+++ src/tp3/GIFInterpreter.java
@@ -1,6 +1,7 @@
 package tp3;
 
 import java.io.*;
+import java.util.Scanner;
 
 /**
  * User: Martin Gutierrez
@@ -13,38 +14,30 @@ public class GIFInterpreter {
         FileInputStream fileInputStream = null;
         FileOutputStream fileOutputStream = null;
 
+        System.out.println("1.RGB to BGR.\n2.Black and White.\n3.Negative\n\nMake your choice:");
 
+        Scanner scanner = new Scanner(System.in);
+        int result = scanner.nextInt();
+        if(result == 1){
+            BGRasRGB(fileInputStream, fileOutputStream);
+        } else if (result == 2){
+            blackAndWhite(fileInputStream, fileOutputStream);
+        } else if (result == 3){
+            negative(fileInputStream, fileOutputStream);
+        }
+
+
+        BGRasRGB(fileInputStream, fileOutputStream);
+    }
+
+    private static void BGRasRGB(FileInputStream fileInputStream, FileOutputStream fileOutputStream) {
         try {
             fileInputStream = new FileInputStream("test.gif");
-            fileOutputStream = new FileOutputStream("result.gif");
+            fileOutputStream = new FileOutputStream("resultBGR.gif");
             int read = fileInputStream.read();
             int i = 0;
             while (read != -1) {
                 if (i >= 12 && i < 780) {
-
-                    //BLANCO Y NEGRO
-
-                    /*int read1 = fileInputStream.read();         //GREEN
-                    i++;
-                    int read2 = fileInputStream.read();              //BLUE
-                    i++;
-                    int result = (read + read1 + read2) / 3;
-                    fileOutputStream.write(result);
-                    fileOutputStream.write(result);
-                    fileOutputStream.write(result);*/
-
-                    //NEGATIVO
-
-                    /*fileOutputStream.write(255 - read);
-                    int read1 = fileInputStream.read();
-                    i++;
-                    fileOutputStream.write(255 - read1);
-                    int read2 = fileInputStream.read();
-                    i++;
-                    fileOutputStream.write(255 - read2);*/
-
-                    //FLASHERO - BGR grabado como RGB
-
                     int read1 = fileInputStream.read();
                     i++;
                     int read2 = fileInputStream.read();
@@ -52,6 +45,72 @@ public class GIFInterpreter {
                     fileOutputStream.write(read2);
                     fileOutputStream.write(read1);
                     fileOutputStream.write(read);
+
+                } else {
+                    fileOutputStream.write(read);
+                }
+                if (i <= 1000) System.out.println(i++ + "\t" + (char) read + "\t" + read);
+                read = fileInputStream.read();
+            }
+            fileOutputStream.flush();
+            fileInputStream.close();
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    private static void blackAndWhite(FileInputStream fileInputStream, FileOutputStream fileOutputStream) {
+
+        try {
+            fileInputStream = new FileInputStream("test.gif");
+            fileOutputStream = new FileOutputStream("resultBW.gif");
+            int read = fileInputStream.read();
+            int i = 0;
+            while (read != -1) {
+                if (i >= 12 && i < 780) {
+                    int read1 = fileInputStream.read();         //GREEN
+                    i++;
+                    int read2 = fileInputStream.read();              //BLUE
+                    i++;
+                    int result = (read + read1 + read2) / 3;
+                    fileOutputStream.write(result);
+                    fileOutputStream.write(result);
+                    fileOutputStream.write(result);
+
+                } else {
+                    fileOutputStream.write(read);
+                }
+                if (i <= 1000) System.out.println(i++ + "\t" + (char) read + "\t" + read);
+                read = fileInputStream.read();
+            }
+            fileOutputStream.flush();
+            fileInputStream.close();
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }private static void negative(FileInputStream fileInputStream, FileOutputStream fileOutputStream) {
+
+        try {
+            fileInputStream = new FileInputStream("test.gif");
+            fileOutputStream = new FileOutputStream("resultNegative.gif");
+            int read = fileInputStream.read();
+            int i = 0;
+            while (read != -1) {
+                if (i >= 12 && i < 780) {
+
+                    fileOutputStream.write(255 - read);
+                    int read1 = fileInputStream.read();
+                    i++;
+                    fileOutputStream.write(255 - read1);
+                    int read2 = fileInputStream.read();
+                    i++;
+                    fileOutputStream.write(255 - read2);
 
                 } else {
                     fileOutputStream.write(read);
